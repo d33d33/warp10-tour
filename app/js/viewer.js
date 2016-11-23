7c2ef776a8b72a642da2d2b9843a1146da0be8bb
d33d33 <kevin@d33d33.fr>
2016-11-23 13:09:06 +0100

    feat(viewer): Add raw view

--- app/js/viewer.js
+++ app/js/viewer.js
@@ -4,9 +4,14 @@ var tw = window.tw;
 $(function () {
   var ans = $('#ans'),
     data = '',
-    graph = false,
+    mode = 'json',
     modeBtn,
-    chart;
+    chart,
+    modes = {
+      'raw': 'json',
+      'json': 'graph',
+      'graph': 'raw'
+    };
 
   tw.viewer = {
     clear: function () {
@@ -33,73 +38,83 @@ $(function () {
       tw.viewer.clear();
       data = json;
 
-      if (graph) {
-        res = json[0];
+      switch (mode) {
+        case 'graph':
+          res = json[0];
 
-        var xs = {},
-          dataset = [];
-        res.forEach(function (gts) {
-          var h = gts.c + JSON.stringify(gts.l);
-          var x = ['x-' + h];
-          var d = [h];
-          gts.v.forEach(function (v) {
-            x.push(Math.round(v[0]));
-            d.push(v[1]);
+          var xs = {},
+            dataset = [];
+          res.forEach(function (gts) {
+            var h = gts.c + JSON.stringify(gts.l);
+            var x = ['x-' + h];
+            var d = [h];
+            gts.v.forEach(function (v) {
+              x.push(Math.round(v[0]));
+              d.push(v[1]);
+            });
+
+            dataset.push(x, d);
+            xs[h] = 'x-' + h;
           });
 
-          dataset.push(x, d);
-          xs[h] = 'x-' + h;
-        });
+          $('#ans').html('<div class="ct-chart" style="position: absolute; top: 0; left: 0; right: 0; bottom:0; overflow: hidden;"><div id="chart"></div></div>');
 
-        $('#ans').html('<div class="ct-chart" style="position: absolute; top: 0; left: 0; right: 0; bottom:0; overflow: hidden;"><div id="chart"></div></div>');
-        modeBtn = $('<button class="button button-outline" style="position: absolute; top: 10px; right: 10px;">json</button>');
-        modeBtn.click(tw.viewer.toggleMode);
-        $('#ans').append(modeBtn);
-
-        chart = c3.generate({
-          bindto: '#chart',
-          data: {
-            xs: xs,
-            columns: dataset
-          },
-          axis: {
-            x: {
-              type: 'indexed' // Values hidden by css
-            }
-          },
-          legend: {
-            show: false
-          },
-          tooltip: {
-            format: {
-              title: function (x) {
-                return moment(x / 1000).toISOString();
+          chart = c3.generate({
+            bindto: '#chart',
+            data: {
+              xs: xs,
+              columns: dataset
+            },
+            axis: {
+              x: {
+                type: 'indexed' // Values hidden by css
+              }
+            },
+            legend: {
+              show: false
+            },
+            tooltip: {
+              format: {
+                title: function (x) {
+                  return moment(x / 1000).toISOString();
+                }
               }
             }
-          }
-        });
+          });
 
-        setTimeout(tw.viewer.resize);
-      } else {
-        var rows = $('<div></div>');
-        json.forEach(function (r, i) {
-          console.log(i);
+          setTimeout(tw.viewer.resize);
+          break;
+        case 'json':
+          var rows = $('<div></div>');
+          json.forEach(function (r, i) {
+            var row = $('<div class="tw-row"></div>');
+
+            var formatter = new JSONFormatter(r, 2);
+            row.append('<div class="row-number">' + (i + 1) + '</div>');
+            row.append(formatter.render());
+            rows.append(row);
+          });
+          ans.html(rows);
+          break;
+        default:
+          var rows = $('<div></div>');
           var row = $('<div class="tw-row"></div>');
+          json.forEach(function (r, i) {
+            console.log(r);
 
-          var formatter = new JSONFormatter(r, 0);
-          row.append('<div class="row-number">' + (i+1) + '</div>');
-          row.append(formatter.render());
-          rows.append(row);
-        });
-        ans.html(rows);
-
-        modeBtn = $('<button class="button button-outline" style="position: absolute; top: 10px; right: 10px;">graph</button>');
-        modeBtn.click(tw.viewer.toggleMode);
-        $('#ans').append(modeBtn);
+            row.append('<div class="row-number">' + (i + 1) + '</div>');
+            row.append(JSON.stringify(r));
+            rows.append(row);
+          });
+          ans.html(rows);
       }
+
+      modeBtn = $('<button class="button button-outline" style="position: absolute; top: 10px; right: 10px;">' + modes[mode] + '</button>');
+      modeBtn.click(tw.viewer.toggleMode);
+      $('#ans').append(modeBtn);
     },
     toggleMode: function () {
-      graph = !graph;
+      mode = modes[mode];
       tw.viewer.json(data); // redraw
     },
     resize: function () {
